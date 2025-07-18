e24ec1520714862cff022eb24f98e1724088aecc
unknown <you@example.com>
2025-07-18 14:24:35 +0600

    posNeg

--- src/main/java/com/digital_nomads/codingbat/Julia/warmup1/newHundred.java
+++
@@ -1,7 +0,0 @@
-package com.digital_nomads.codingbat.Julia.warmup1;
-
-public class newHundred {
-    public boolean nearHundred(int n) {
-        return ( n >= 90 && n <= 110) || ( n >= 190 && n <= 210);
-    }
-}
