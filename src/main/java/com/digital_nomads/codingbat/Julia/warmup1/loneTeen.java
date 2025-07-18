007c7aa351fb21521b35a2d0bd1f9e549e0efb08
unknown <you@example.com>
2025-07-18 14:39:26 +0600

    loneTeen

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/loneTeen.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class loneTeen {
+    public boolean loneTeen(int a, int b) {
+        return (a >= 13 && a <= 19) ^ (b >= 13 && b <= 19);
+    }
+}
