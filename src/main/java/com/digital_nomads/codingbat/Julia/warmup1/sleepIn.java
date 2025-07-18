47eb3a2c78c00dafba76ebdb9681aecc471241e5
unknown <you@example.com>
2025-07-18 11:45:31 +0600

    SleepIn

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/sleepIn.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class sleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+}
