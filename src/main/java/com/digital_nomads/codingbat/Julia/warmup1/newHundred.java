22ff9143202f3613ea966d6ee83b35e7b7d1a21a
unknown <you@example.com>
2025-07-18 14:21:33 +0600

    nearHundred

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/newHundred.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class newHundred {
+    public boolean nearHundred(int n) {
+        return ( n >= 90 && n <= 110) || ( n >= 190 && n <= 210);
+    }
+}
