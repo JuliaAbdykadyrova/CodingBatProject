1203867507836e6e8f5907292b7019e88d73703a
unknown <you@example.com>
2025-07-18 14:33:59 +0600

    backAround

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/backAround.java
@@ -0,0 +1,9 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class backAround { public String backAround(String str) {
+    String last = str.substring(str.length()-1);
+
+    return last+str+last;
+
+}
+}
