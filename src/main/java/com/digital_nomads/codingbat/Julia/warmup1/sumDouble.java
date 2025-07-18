539999732e600d4b5ce0f37fe72e3b6c209d82a6
unknown <you@example.com>
2025-07-18 11:48:12 +0600

    sumDouble

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/sumDouble.java
@@ -0,0 +1,9 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class sumDouble { public int sumDouble(int a, int b) {
+    if (a == b) {
+        return a*4;
+    } else {
+        return a+b;}
+}
+}
