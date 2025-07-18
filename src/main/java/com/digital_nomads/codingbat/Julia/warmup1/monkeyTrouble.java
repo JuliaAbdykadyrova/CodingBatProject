49bbb936cd89c9b13c1fcfcdf741089f9d7c565a
unknown <you@example.com>
2025-07-18 11:47:13 +0600

    monkeyTrouble

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/monkeyTrouble.java
@@ -0,0 +1,6 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class monkeyTrouble { public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    return aSmile & bSmile || !aSmile & !bSmile;
+}
+}
