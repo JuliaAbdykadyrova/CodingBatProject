ba37866845cfbf7e0dde7aeded11c8af4b30b4a9
unknown <you@example.com>
2025-07-18 12:00:36 +0600

    sumDouble

--- src/main/java/com/digital_nomads/codingbat/Julia/warmup1/sumDouble.java
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/sumDouble.java
@@ -1,6 +1,7 @@
 package com.digital_nomads.codingbat.Julia.warmup1;
 
-public class sumDouble { public int sumDouble(int a, int b) {
+public class sumDouble {
+    public int sumDouble(int a, int b) {
     if (a == b) {
         return a*4;
     } else {
