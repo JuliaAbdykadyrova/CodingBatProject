a4d0907c60798b00d7a2032bc5bb3035259eb354
unknown <you@example.com>
2025-07-18 12:03:13 +0600

    diff21

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/diff21.java
@@ -0,0 +1,10 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class diff21 { public int diff21(int n) {
+    if ( n> 21) {
+        return (n - 21) *2;
+    } else {
+        return (21 - n);
+    }
+}
+}
