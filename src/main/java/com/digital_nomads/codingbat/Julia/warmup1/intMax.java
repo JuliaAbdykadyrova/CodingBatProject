c975003ea4f35a31015fba9d414eb84dbe9a915f
unknown <you@example.com>
2025-07-18 14:42:05 +0600

    intMax

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/intMax.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class intMax {
+    public int intMax(int a, int b, int c) {
+    int max = Math.max(a, Math.max(b, c));
+    return max;
+}
+}
