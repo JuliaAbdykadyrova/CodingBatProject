62d7c24dc02c9391b462984220813e306d1959b4
unknown <you@example.com>
2025-07-18 12:04:32 +0600

    parrotTrouble

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/parrotTrouble.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class parrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+    return talking &&  ( hour <7 || hour > 20 );
+}
+}
