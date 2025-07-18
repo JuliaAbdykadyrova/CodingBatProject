885c1c61305aa2172ecb27d7218b5a060be1fd4f
unknown <you@example.com>
2025-07-18 14:43:21 +0600

    in3050

---
+++ src/main/java/com/digital_nomads/codingbat/Julia/warmup1/in3050.java
@@ -0,0 +1,6 @@
+package com.digital_nomads.codingbat.Julia.warmup1;
+
+public class in3050 { public boolean in3050(int a, int b) {
+    return ( a >=30 && a <= 40 && b >=30 && b <= 40) || ( a >=40 && a <= 50 && b >=40 && b <= 50);
+}
+}
